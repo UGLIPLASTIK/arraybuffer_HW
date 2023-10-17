5da210d8df7a4b2a620c2bbca8d50b107f792182
Dmitry.Kononets <d.cononec@gmail.com>
2023-10-17 18:26:39 +0300

    1 задача

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@
 import Daemon from './js/class/daemon';
 import ArrayBufferConverter from './js/class/array-buffer';
 import getBuffer from './js/get-buffer';
+
+const daemon = new Daemon('Biba');
+daemon.distance = 3;
+daemon.setAttack(100);
+daemon.setStoned(true);
+console.log(daemon);
--- src/js/class/math.js
+++ src/js/class/math.js
@@ -5,43 +5,15 @@ export default class Math extends Character {
   constructor(name, type) {
     super(name, type);
     this.defence = 40;
-    this.attack = 100;
-    this.stoned = false;
+    this.distance = undefined;
   }
 
-  getAttack() {
-    return this.attack;
+  setAttack(attack) {
+    this.attack = attack - (attack / 10) * (this.distance - 1);
   }
 
-  setAttack(cellsFromEnemy) {
-    // if (this.stoned) {
-    //   this.attack -= Math.round(Math.log2(cellsFromEnemy) * 5);
-    // }
-    if (cellsFromEnemy === 1) return;
-    if (cellsFromEnemy === 2) this.attack = (this.attack / 100) * 90;
-    if (cellsFromEnemy === 3) this.attack = (this.attack / 100) * 80;
-    if (cellsFromEnemy === 4) this.attack = (this.attack / 100) * 70;
-    if (cellsFromEnemy === 5) this.attack = (this.attack / 100) * 60;
-    if (cellsFromEnemy === 6) this.attack = (this.attack / 100) * 50;
-    if (cellsFromEnemy === 7) this.attack = (this.attack / 100) * 40;
-    if (cellsFromEnemy === 8) this.attack = (this.attack / 100) * 30;
-    if (cellsFromEnemy === 9) this.attack = (this.attack / 100) * 20;
-    if (cellsFromEnemy === 10) this.attack = (this.attack / 100) * 10;
-    // let percent = 100;
-    // for (let i = 0; i < cellsFromEnemy; i++) {
-    //   percent -= 10;
-    //   if (i === cellsFromEnemy) {
-    //     this.attack = (this.attack / 100) * percent;
-    //     break;
-    //   }
-    // }
-  }
-
-  getStoned() {
-    return this.stoned;
-  }
-
-  setStoned() {
-    if (this.stoned === false) this.stoned = true;
+  setStoned(data) {
+    if (typeof (data) !== 'boolean') return;
+    this.stoned = data;
   }
 }
